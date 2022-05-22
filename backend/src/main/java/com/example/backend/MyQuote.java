95cd84993c77183ae6902ad5bd1706970f6d3287
Katarzyna Sobczyszyn <katarzyna.sobczyszyn@gmail.com>
2022-05-22 16:06:29 +0200

    Quote

---
+++ backend/src/main/java/com/example/backend/MyQuote.java
@@ -0,0 +1,82 @@
+package com.example.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class MyQuote {
+    private @Id
+    @GeneratedValue
+    Long id;
+    private String quote;
+    private String author;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private String category;
+
+    public MyQuote() {
+
+    }
+
+    public MyQuote(String quote, String author) {
+        this.quote = quote;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    @Override
+    public String toString() {
+        return "MyQuote{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                ", author='" + author + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyQuote myQuote = (MyQuote) o;
+        return Objects.equals(id, myQuote.id) && Objects.equals(quote, myQuote.quote) && Objects.equals(author, myQuote.author) && Objects.equals(category, myQuote.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, quote, author, category);
+    }
+}
