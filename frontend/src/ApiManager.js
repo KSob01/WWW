2ce9ece068741b3d2554b5a2668f05f97f0cb0b2
Katarzyna Sobczyszyn <72964417+KSob01@users.noreply.github.com>
2022-05-23 19:35:37 +0200

    quote fetch - without Component

--- frontend/src/ApiManager.js
+++ frontend/src/ApiManager.js
@@ -1,21 +1,12 @@
-import {Component} from "react";
-
-export class ApiManager extends Component {
-    static myInstance = null;
-    baseURL = "http://localhost:8080"
-    static getInstance() {
-        return new ApiManager();
-    }
-    async getAll() {
-        const res = await fetch(this.baseURL + '/quotes');
-        const json = await res.json();
-        console.log(json);
-        return json;
-    }
-    async getByID (id){
-        const res = await fetch(this.baseURL +'/quote/'+id);
-        return  await res.json();
-    }
+const baseURL = "http://localhost:8080"
+export const getAll = async () => {
+    const res = await fetch(baseURL + '/quotes');
+    const json = await res.json();
+    console.log(json);
+    return json;
 }
-export default ApiManager
+export const getByID = async (id) => {
+    const res = await fetch(baseURL + '/quote/' + id);
+    return await res.json();
+}
 
--- frontend/src/quote-of-the-day.js
+++ frontend/src/quote-of-the-day.js
@@ -1,34 +1,34 @@
-import ApiManager from "./ApiManager";
+import {getByID} from "./ApiManager";
 import React, {Component, useEffect} from 'react';
 
-
-
-
-export default class QuoteApp extends Component{
+export default class QuoteApp extends Component {
     constructor(props) {
         super(props)
         this.state = {
             resultArr: []
         }
     }
-    callApi() {
-        ApiManager.getInstance().getByID(1).then((result) => {
-            this.setState({ resultArr: result});
-        }).catch((err)=>console.log(err));
-    }
-render() {
-    this.callApi()
-    return (
-        <div>
-            <div className="QuoteApp">
-                <header className="App-header">
-                    <p>
-                        Service  : quote-of-the-day.js
-                    </p>
-                </header>
-            </div>
-                <p>{JSON.stringify(this.state.resultArr)}</p>
-        </div>
 
-)}
+    callApi() {
+        getByID(1).then((result) => {
+            this.setState({resultArr: result});
+        }).catch((err) => console.log(err));
+    }
+
+    render() {
+        this.callApi()
+        return (
+            <div>
+                <div className="QuoteApp">
+                    <header className="App-header">
+                        <p>
+                            Service : quote-of-the-day.js
+                        </p>
+                    </header>
+                </div>
+                <p>{JSON.stringify(this.state.resultArr)}</p>
+            </div>
+
+        )
+    }
 }
